90ccf6f2ba259d05bb697429a06edf7f8fe9aefd
Paulina <mail@wp.pl>
2017-09-27 17:42:32 +0200

    First commit

---
+++ src/main/java/org/pethotel/HeavenForPets/HeavenForPetsApplication.java
@@ -0,0 +1,12 @@
+package org.pethotel.HeavenForPets;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HeavenForPetsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HeavenForPetsApplication.class, args);
+	}
+}
