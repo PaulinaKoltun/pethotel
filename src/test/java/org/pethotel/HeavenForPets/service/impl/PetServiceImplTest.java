b2ddb9c93ef3775a002e038949b90a738bea5303
Paulina <paulina.k7@poczta.fm>
2018-05-15 17:23:46 +0200

    new tests ;/

--- src/test/java/org/pethotel/HeavenForPets/service/impl/PetServiceImplTest.java
+++ src/test/java/org/pethotel/HeavenForPets/service/impl/PetServiceImplTest.java
@@ -1,17 +1,28 @@
 package org.pethotel.HeavenForPets.service.impl;
 
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Incubating;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
+import org.pethotel.HeavenForPets.domein.Pet;
+import org.pethotel.HeavenForPets.domein.Rooms.Room;
+import org.pethotel.HeavenForPets.entity.PetEntity;
+import org.pethotel.HeavenForPets.entity.RoomEntity;
 import org.pethotel.HeavenForPets.mappers.PetMap;
 import org.pethotel.HeavenForPets.repository.PetRepository;
 import org.pethotel.HeavenForPets.service.OwnerService;
 import org.pethotel.HeavenForPets.service.RoomService;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
 import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
 
 
 @RunWith(MockitoJUnitRunner.class)
@@ -33,6 +44,56 @@ public class PetServiceImplTest {
     private PetServiceImpl petService;
 
 
+    @Test
+    public void getPets(){
+        List<PetEntity> petEntities = new ArrayList<>();
 
+        when(petRepository.findAll()).thenReturn(petEntities);
+
+        petService.getPets();
+
+        verify(petRepository, times(1)).findAll();
+    }
+
+    @Test
+    public void bringPetAgain(){
+        Pet pet = new Pet();
+        PetEntity petEntity = new PetEntity();
+        RoomEntity roomEntity = new RoomEntity();
+
+        when(petRepository.findOne(pet.getId())).thenReturn(petEntity);
+        when(roomService.getRoomByNumber(pet.getRoomNumber())).thenReturn(roomEntity);
+        when(petMap.map(pet, petEntity, roomEntity)).thenReturn(petEntity);
+        when(petRepository.save(petEntity)).thenReturn(petEntity);
+
+        petService.bringPetAgain(pet);
+
+        verify(petRepository, times(1)).save(petEntity);
+    }
+
+    @Test
+    public void pickUpPets(){
+        List<Integer> idList = new ArrayList<>();
+        Integer id = new Integer(2);
+        idList.add(id);
+        PetEntity petEntity = new PetEntity();
+        RoomEntity roomEntity = new RoomEntity();
+        roomEntity.setPrice(BigDecimal.TEN);
+        roomEntity.setFreePlaces(12);
+        petEntity.setRoomEntity(roomEntity);
+        petEntity.setDateIn(new Date(12/12/2012));
+        petEntity.setDateOut(new Date(14/12/2012));
+
+        when(petRepository.ownerEntityList(idList)).thenReturn(idList);
+        when(petRepository.findOne((long) id)).thenReturn(petEntity);
+        when(petRepository.save(petEntity)).thenReturn(petEntity);
+
+        petService.pickupPets(idList);
+
+        verify(petRepository,times(1)).save(petEntity);
+    }
+
+    @Test
+    public void 
 
 }
